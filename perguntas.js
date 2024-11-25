09baaebf5fbea034ae3c66049f25af602b77c7bf
Flaviafreitas10 <flaviafreitasfreitasbalestri@gmail.com>
2024-11-25 12:05:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Biologia',
+    'Quantos ossos o ser humano tem no corpo?',
+    'O corpo do ser humano tem 206 ossos, sendo 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros superiores. '
+)
+
+criaCartao(
+    'Hstória',
+    'O que representam os cinco anéis olímpicos?',
+    'As partes do mundo unidas pelo Olimpismo, a bandeira olímpica é branca e contém cinco anéis, cada um com uma cor diferente. Esses anéis são representados nas seguintes posições: na parte de cima - da esquerda para a direita - estão os anéis azul, preto e vermelho; na parte de baixo - da esquerda para a direita - estão os anéis amarelo e verde.'
+)
+
+criaCartao(
+    'Química',
+    'QUal o nome popular do coreto de sódio?',
+    'Cloreto de sódio (NaCl) é popularmente conhecido como sal de cozinha, porque é o sal que ultilizamos para dar mais saboraos alimentos.'
+)
+
+criaCartao(
+    'Esportes',
+    'Depois do futebol, qual o esporte mais popular no Brasil?',
+    'O segundo esporte mais popular do mundo é o vôlei, que chegou ao Brasil em 1915. Sendo assim a seleção brasileira de vôlei é uma das melhores do mundo.'
+)
+
+criaCartao(
+    'Fultebol',
+    'Em qual ano o Corinthians guanhou seu primeiro título Brasileiro?',
+    'Em 1990 o Corinthians sagrou-se campeão nacional, ganhand do São Paulo na final daquele ano.'
+)
+
+criaCartao(
+    'Fultebol',
+    'Em quais anos o Corinthians sagrou-se campeão Brasileiro da Série A?',
+    'Nos anos de 1990,1998,1999,2005,2011 e 2015, foram os 6 títulos Brasileirs da hístoria corintiana'
+)
